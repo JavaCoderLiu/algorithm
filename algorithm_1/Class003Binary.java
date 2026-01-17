9b424a29145fd3e1485ac81fd743c860f900e16c
JavaCoderLiu <liuxueqian@qzdatasoft.com>
2026-01-17 13:45:22 +0800

    算法003二进制和位运算

---
+++ algorithm_1/Class003Binary.java
@@ -0,0 +1,13 @@
+public class Class003Binary {
+    public static void printBinary(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printBinary(88);
+        printBinary(-8);
+    }
+}
