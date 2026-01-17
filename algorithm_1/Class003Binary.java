a6aa9410439e2ddda27b4408ddb119df1cf1f351
JavaCoderLiu <liuxueqian@qzdatasoft.com>
2026-01-17 14:03:36 +0800

    算法003二进制和位运算 补充注释

--- algorithm_1/Class003Binary.java
+++ algorithm_1/Class003Binary.java
@@ -1,4 +1,8 @@
 public class Class003Binary {
+    /**
+     * 打印int类型的二进制表示
+     * @param num int整数
+     */
     public static void printBinary(int num) {
         for (int i = 31; i >= 0; i--) {
             System.out.print((num & (1 << i)) == 0 ? "0" : "1");
