4bbe7a196e69ba35c78b36b752d433032d6307ec
JavaCoderLiu <liuxueqian@qzdatasoft.com>
2026-03-20 22:11:57 +0800

    算法34链表高频题目和必备技巧 题目5:返回链表的第一个入环节点。快慢指针找中点。

---
+++ algorithm_1/class34/Code05_LinkedListCycleII.java
@@ -0,0 +1,35 @@
+package class34;
+
+public class Code05_LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while (slow != fast) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
